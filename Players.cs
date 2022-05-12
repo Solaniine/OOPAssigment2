e8b8fb90c0f8c7635f66dcd1e3e4a288a7150694
Owen Welsh <100965418+Solaniine@users.noreply.github.com>
2022-05-12 14:48:11 +0100

    Add files via upload

---
+++ Players.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment2
+{
+    internal class Players
+    {
+        private int playerID;
+        private string playername = "";
+        private int playerscore;
+        int numberofplayers;
+        public void player(int id, string name, int score)
+        {
+            playerID = id;
+            playername = name;
+            playerscore = score;
+            
+
+           
+        }
+        public void playeramount()
+        {
+            
+            bool amountofplayers1to10 = true;
+
+            //forces int values only 
+            while (!int.TryParse(Console.ReadLine(), out numberofplayers))
+            {
+                Console.Write("This is not valid input. Please enter an integer value: ");
+            }
+            //amount of players between 2 and 10
+            while (amountofplayers1to10 == true)
+            {
+
+
+                while (numberofplayers <= 1)
+                {
+                    Console.WriteLine("you can not have one or below 1 players");
+                    numberofplayers = int.Parse(Console.ReadLine());
+                }
+                while (numberofplayers > 11)
+                {
+                    Console.WriteLine("you can not have above 10 players ");
+                    numberofplayers = int.Parse(Console.ReadLine());
+                }
+
+
+                while (numberofplayers < 11)
+                {
+        
+                    Console.WriteLine("number of people playing: " + numberofplayers);
+                    amountofplayers1to10 = false;
+                    break;
+                }
+
+
+
+            }
+
+            
+        }
+        public void playernames()
+        {
+
+            bool playernamed = true;
+
+            while (playernamed == true)
+            {
+
+
+
+
+            }
+
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Assigment2
+{
+    class theProgram
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Rules \r\n Players in turn roll all five dice and score for three-of-a-kind or better. \r\n If a player only has two-of-a-kind, \r\n they may re-throw the remaining dice in an attempt to improve the matching dice values. \r\n If no matching numbers are rolled, a player scores 0. \r\n");
+            
+            Players players = new Players();
+            players.playeramount();
+            int numberofplayers = int.Parse(Console.ReadLine());
+            Console.WriteLine(numberofplayers);
+          //  players.playernames();
+
+
+
+        }
+    }
+}
+
