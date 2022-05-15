ad547d6edb51611156a55e0baf2d0a051916457f
Owen Welsh <100965418+Solaniine@users.noreply.github.com>
2022-05-15 13:02:11 +0100

    Dice

--- Dice.cs
+++ Dice.cs
@@ -8,11 +8,21 @@ namespace Assigment2
 {
     internal class Dice
     {
-        int numofDice;
-        int maxnumofDice;
-        int minnumofDice;
-        int numberofrolls;
+        private int dieId;
+        private int dieFaces;
 
+
+        public static List<int> roll_dice()
+        {
+            List<int> roll = new List<int>();
+            Random random = new Random();
+            for (int i = 0; i < 6; i++)
+            {
+                roll.Add(random.Next(1, 7));
+            }
+
+            return roll;
+        }
         
     }
 }
