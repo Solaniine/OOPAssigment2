5a41e2380ab90929bcdb369cd8afc280ec61e4eb
Owen Welsh <100965418+Solaniine@users.noreply.github.com>
2022-05-18 02:14:09 +0100

    done

--- Game.cs
+++ Game.cs
@@ -66,7 +66,7 @@ namespace Assigment2
                 {
                     // diceface = item in list now equals dice roll 
                     int dieface = diceList[i].Returndiceroll();
-                    // throwdie adds diceroll
+                    // throwdie add to diceroll
                     throwdie.Add(dieface);
                     //shows counter + the roll value 
                     Console.WriteLine("Dice " + (couter + 1) + " " + dieface + " ");
@@ -77,31 +77,105 @@ namespace Assigment2
         }
         public static void Play()
         {
-
+            //playeramount is made into playlist
             List<Player> playerList = playeramount();
+            //allows for a break 
             while (true)
             {
+                //each player inside of the playeramount is used here
                 foreach (var player in playerList)
                 {
-                    Console.WriteLine("It's " + player.PlayerName + "to roll \r\nPress enter to roll");
+                    //roll by pressing enter
+                    Console.WriteLine(player.PlayerName + " Enter, to roll again ");
+                    //reads key stroke
                     Console.ReadKey();
+                    //array for thrown dice, 0, player
                     GetScore(ThrowDice(), 0, player);
-                    Console.WriteLine(player.PlayerName + "'s Score: " + player.Playerscore);
+                    //name + score+ players score
+                    Console.WriteLine(player.PlayerName + " Score: " + player.Playerscore);
+                    //if playerscore = 50 then game is over 
                     if (player.Playerscore >= 50)
                     {
-                        Console.WriteLine("Congrats, " + player + "won!");
-                        break;
+                        //kills console when player won 
+                        Console.WriteLine(player + "won!");
+                        Thread.Sleep(5000);
+                        System.Environment.Exit(1);
+
+
                     }
                 }
             }
         }
 
-        
+        private static void GetScore(List<int> dieValues, int counter, Player player)
+        {
+            int score = 0;
+            // creates a dictinonary for the diceroll with key values of pairs. 
+            Dictionary<int, int> diceThrow = new Dictionary<int, int>(6)
+            {
+                {1,0},
+                {2,0},
+                {3,0},
+                {4,0},
+                {5,0},
+                {6,0}
+                };
+            //for every value inside of the dice values +1
+            foreach (int Value in dieValues)
+            {
+                diceThrow[Value]++;
+            }
+            //allows for the ai to roll and rolls from 1 to maxvalue(6)
+            int AiValue = diceThrow.Values.Max();
+            Console.WriteLine("AI value is: " + AiValue);
+            //this is the timing of the numbers 
+            if (AiValue == 5)
+            {
+                player.Player_scoresupdate(12);
+            }
+            else if (AiValue == 4)
+            {
+                player.Player_scoresupdate(6);
+            }
+            else if (AiValue == 3)
+            {
+                player.Player_scoresupdate(3);
+            }
+            else if (AiValue == 2)
+            {
+                
+                if (counter < 1)
+                {
+                    //allows for roll for the 3rd die
+                    int repeated = 0;
+
+                    foreach (var pair in diceThrow)
+                    {
+                        // if equal to 2 then store both values next to each other 
+                        if (pair.Value == 2)
+                        {
+                            repeated = pair.Key;
+                        }
+                    }
+                   
+                    Console.WriteLine(repeated + "twice repeated, press enter\n");
+                    Console.ReadKey();
+                    counter++;
+
+                    List<int> newList = Reroll(repeated);
+                    GetScore(newList, counter, player);
+                }
+            }
+            else
+            {
+                Console.WriteLine("AI won sorry bud");
+            }
+        }
         private static List<Player> playeramount()
         {
-
+            //number of players playing 
             int numberofplayers = 0;
-
+            // allows playerlist to interact
             List<Player> playerlist = new List<Player>();
             //forces int values only 
            
@@ -112,7 +186,7 @@ namespace Assigment2
                 {
                     Console.Write("This is not valid input. Please enter an integer value: ");
                 }
-
+                //below 2 or above 11 then repeats playeramount till between values 
                 if (numberofplayers < 2 || numberofplayers > 11)
                 {
                     Console.WriteLine("The max number of players is 11 and the least is 2");
@@ -128,76 +202,24 @@ namespace Assigment2
             List<string> listofplayers = new List<string> { };
 
                 Console.WriteLine("now enter each players name");
+            //every player can now input their name from range 0 to the amount of players
             foreach (int i in Enumerable.Range(0, numberofplayers))
                 {
+                //id + 1
+
                     int playerID = i + 1;
+                //adds values to list of players
                     listofplayers.Add(Console.ReadLine());
-                    string playerName = Console.ReadLine();
+                //name == user input
+                string playerName = Console.ReadLine();
+                //adds id, name and id to the list
                     playerlist.Add(new Player(playerID, playerName, 0));
                 }
+            //returns it back 
             return playerlist;
 
             
         }
-        private static void GetScore(List<int> dieValues, int counter, Player player)
-        {
-            int score = 0;
-            Dictionary<int, int> diceThrow = new Dictionary<int, int>(6)
-            {
-                {1,0},
-                {2,0},
-                {3,0},
-                {4,0},
-                {5,0},
-                {6,0}
-                };
-            foreach (int dieValue in dieValues)
-            {
-                diceThrow[dieValue]++;
-            }
-
-            int maxValue = diceThrow.Values.Max();
-            Console.WriteLine("Max value is: " + maxValue);
-
-            if (maxValue == 5)
-            {
-                player.Player_scoresupdate(12);
-            }
-            else if (maxValue == 4)
-            {
-                player.Player_scoresupdate(6);
-            }
-            else if (maxValue == 3)
-            {
-                player.Player_scoresupdate(3);
-            }
-            else if (maxValue == 2)
-            {
-                if (counter < 1)
-                {
-                
-                    int repeatedValue = 0;
-
-                    foreach (var pair in diceThrow)
-                    {
-                        if (pair.Value == 2)
-                        {
-                            repeatedValue = pair.Key;
-                        }
-                    }
-
-                    Console.WriteLine(repeatedValue + " is repeated twice.\nPress enter to re-roll.......");
-                    Console.ReadKey();
-                    counter++;
-
-                    List<int> newList = Reroll(repeatedValue);
-                    GetScore(newList, counter, player);
-                }
-            }
-            else
-            {
-                Console.WriteLine("Better luck next time!");
-            }
-        }
+  
     }
 }
--- Program.cs
+++ Program.cs
@@ -13,11 +13,6 @@ namespace Assigment2
             Console.WriteLine("Rules \r\n Players in turn roll all five dice and score for three-of-a-kind or better. \r\n If a player only has two-of-a-kind, \r\n they may re-throw the remaining dice in an attempt to improve the matching dice values. \r\n If no matching numbers are rolled, a player scores 0. \r\n");
             Console.WriteLine("input the number of players");
             Game.Play();
-
-
-            RollingDie die = new RollingDie();
-            Console.WriteLine(die.Returndiceroll());
-
         }
     }
 }
